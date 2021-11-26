77d474892bdeb2839dbc9944b8b67517758f3aa0
Wilkin Novo <w@novo.ws>
2021-11-26 13:50:41 -0500

    feat: added nav

--- src/features/nav/Nav.js
+++ src/features/nav/Nav.js
@@ -1,9 +1,23 @@
+import { Link, useLocation } from 'react-router-dom';
 import css from './Nav.module.css';
 
 export default function Nav() {
+  const { pathname } = useLocation();
+
   return (
     <div className={css.nav}>
       <h1>Logo</h1>
+
+      <div className={css.menu}>
+        <ul>
+          <li className={pathname === '/' ? css.active : ''}>
+            <Link to="/">Start</Link>
+          </li>
+          <li className={pathname === '/add' ? css.active : ''}>
+            <Link to="/add">Add</Link>
+          </li>
+        </ul>
+      </div>
     </div>
   );
 }
