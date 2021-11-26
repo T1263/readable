694ca52f89c41660ef2c8216a0c756f5ab72a4b7
Wilkin Novo <w@novo.ws>
2021-11-26 14:47:52 -0500

    feat: page by category

--- src/app/pages/category/index.js
+++ src/app/pages/category/index.js
@@ -1,3 +1,20 @@
+import { useEffect, useState } from 'react';
+import { useSelector } from 'react-redux';
+import { useParams } from 'react-router';
+import PostsList from '../../../features/posts/List';
+import css from './Category.module.css';
 export default function Categories() {
-  return <h1>Category Page</h1>;
+  const { category } = useParams();
+  const posts = useSelector(({ posts }) => posts.list);
+  const [thePosts, setThePosts] = useState([]);
+
+  useEffect(() => {
+    const filteredPost = posts.filter((post) => post.category === category);
+    setThePosts(filteredPost);
+  }, [posts, category]);
+  return (
+    <div className={css.category}>
+      <PostsList posts={thePosts} />
+    </div>
+  );
 }
