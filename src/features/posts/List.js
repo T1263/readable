51074003b2f8aa985d8aeb4c67d2f77d99212ac8
Wilkin Novo <w@novo.ws>
2021-11-26 11:19:04 -0500

    feat: start post details

--- src/features/posts/List.js
+++ src/features/posts/List.js
@@ -1,6 +1,30 @@
+import { Link } from 'react-router-dom';
 import css from './List.module.css';
+
 export default function List({ posts }) {
-  const Post = ({ title }) => <li>{title}</li>;
+  console.log(posts);
+  const Post = ({ title, voteScore, commentCount, category, id }) => (
+    <li className={css.post}>
+      <div className={css.data}>
+        <span className={css.score}>
+          <h4>Score</h4>
+          <h4> {voteScore}</h4>
+        </span>
+        <span className={css.comments}>
+          <h4>Comments</h4>
+          <h4> {commentCount}</h4>
+        </span>
+      </div>
+      <div>
+        <h2>
+          <Link to={`/${category}/${id}`}>{title}</Link>
+        </h2>
+        <p>
+          Category: <Link to={`/${category}`}>{category}</Link>
+        </p>
+      </div>
+    </li>
+  );
   return (
     <div className={css.posts}>
       <ul>
