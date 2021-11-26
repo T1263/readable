90ee4ffde629dd28dfd41a64dad02eb5a6b685aa
Wilkin Novo <w@novo.ws>
2021-11-26 15:06:38 -0500

    feat: post page starter

--- src/app/pages/category/post.js
+++ src/app/pages/category/post.js
@@ -1,3 +1,23 @@
+import { useEffect, useState } from 'react';
+import { useSelector } from 'react-redux';
+import { useParams } from 'react-router';
+
 export default function Post() {
-  return <h1>Post Page</h1>;
+  const { postId } = useParams();
+  const posts = useSelector(({ posts }) => posts.list);
+  const [thePost, setThePost] = useState({ title: '' });
+
+  useEffect(() => {
+    let post = posts.filter((post) => post.id === postId);
+    if (post.length > 0) setThePost(post[0]);
+  }, [posts, postId]);
+
+  const { title, body, author } = thePost;
+  return (
+    <div>
+      <h2> {title}</h2>
+      <h2>{body}</h2>
+      <h3>{author}</h3>
+    </div>
+  );
 }
