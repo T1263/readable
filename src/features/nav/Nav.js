1550696b38266f035a37f287f8f7c7116df474c7
Wilkin Novo <w@novo.ws>
2021-11-29 21:19:12 -0500

    feat: when loading... replaces logo

--- src/features/nav/Nav.js
+++ src/features/nav/Nav.js
@@ -1,12 +1,13 @@
+import { useSelector } from 'react-redux';
 import { Link, useLocation } from 'react-router-dom';
 import css from './Nav.module.css';
 
 export default function Nav() {
   const { pathname } = useLocation();
-
+  const loading = useSelector(({ posts }) => posts.loading);
   return (
     <div className={css.nav}>
-      <h1>Readable</h1>
+      {loading ? <h1>...loading.</h1> : <h1>Readable</h1>}
 
       <div className={css.menu}>
         <ul>
