3ff5711c67b99c6327127615bba7c7db6977d8a0
Wilkin Novo <w@novo.ws>
2021-11-30 19:09:58 -0500

    fix: Live Heroku server.

--- src/app/shared.js
+++ src/app/shared.js
@@ -1,5 +1,5 @@
-export const API_URL = 'http://localhost:3001';
-
+// export const API_URL = 'http://localhost:3001'; Local ~Clone the server. See readme
+export const API_URL = 'https://secret-tundra-15648.herokuapp.com'; // Live Heroku Server
 export const fetchOptions = {
   method: 'GET',
   headers: {
